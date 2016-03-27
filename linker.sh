142c393518cd422e8bf309efe37d4c7dfd962ee9
Todd Kaufmann <toddkaufmann+github@gmail.com>
2016-03-27 17:23:10 -0400

    check if anything exists, not just file, because we link anything that is +x (ie, dirs too) so need to test for that.  Bug or feature ?

--- linker.sh
+++ linker.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-# in some cases it's better to have multiple different dirs on your path.
-# that could require updating many shell/environments (maybe), logout/login.
-# .. so maybe best of both works is to link temporarily, and add paths for later.
+# Create symbolic links to executable files in multiple directories.
+# The file in $DIRLIST should contain a list of directories.
+# $DIRLIST defaults to 'linker.dirs' in the current directory but can be overridden on command line.
+
+# In some cases it's better to have multiple different dirs on your path.
+# That might require updating many shell/environments, logout/login, etc.
+# Maybe best of both worlds is to link temporarily, and add paths for later.
 # 
 
-# notes:
-# - only +x scripts;  some (mine) depend on data being in the same dir where the executable is;
-# if you link the script you will need to link in the data (manually)
+# Notes:
+# - only +x scripts;  some scripts may depend on data being in the same dir where the executable is;
+# if you link the script you may need to link in the data manually
+# (depends how the script is written).
 #
-# if there's any name overlap in your dirs, it's first come, first serve.
+# If there's any name overlap in your dirs, it's first come, first serve.
 
 
-# default name, but can be overridden in the env.
+# Default name, but can be overridden in the env.
 # this file has one dir per line.  '#' at beginning of line will comment it out.
 : ${DIRLIST:="linker.dirs"}
 #
@@ -47,14 +52,14 @@ grep -v '^#' "$DIRLIST" | \
 	 for f in "$dir"/* ; do 
 	     if [ -x "$f" ] ;  then
 		 base=$(basename "$f")
-		 if [ ! -f "$base" ]; then
+		 if [ ! -e "$base" ]; then
 		     if ln -s "$f" . ; then
 			 echo "added: $f"; 
 			 log "rm ./'$base'"
 		     else
 			 lnstatus=$?
 			 echo "error linking ($lnstatus)"
-			 log  "error linking $f ($lnstatus)"
+			 log  "# error linking $f ($lnstatus)"
 		     fi
 		 else 
 		     log "#  .. is already here:  $f"
@@ -68,7 +73,8 @@ grep -v '^#' "$DIRLIST" | \
      fi
 done 
 
-echo '# you can undo all these links with by executing this script:'
+echo
+echo '# you can undo all these links (if any) by executing this script:'
 echo "sh $LINKING_LOG"
 
 
