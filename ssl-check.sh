df2524e02589a2ce5f7931b01ce6945d7e4151e8
Todd Kaufmann <toddkaufmann+g@gmail.com>
2017-07-04 22:20:17 -0400

    Find when cerificate for a domain expires.

---
+++ ssl-check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+check_ssl () 
+{ 
+    local dom="$1";
+    echo;
+    echo ===== checking cert for "$dom";
+    echo;
+    openssl s_client -servername $dom -connect $dom:443 < /dev/null 2> /dev/null | openssl x509 -noout -dates;
+    echo;
+    echo ======== show the chain;
+    echo;
+    openssl s_client -showcerts -connect $dom:443 < /dev/null 2>&1 > /dev/null;
+    echo
+}
+
+for dom in $*; do
+    check_ssl "$dom"
+done
