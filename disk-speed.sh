920e0478c1268f15207971684cf93bc44f0b110b
Todd Kaufmann <toddkaufmann+g@gmail.com>
2018-03-28 11:37:17 -0400

    does dd like m or M for megabytes?  try to figure out block device.

--- disk-speed.sh
+++ disk-speed.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # As root..
 dev=$1
@@ -7,4 +7,30 @@ dev=$1
 blocksize=$[ 1024 * 1024 ]
 count=600
 
-time dd if=/dev/r$dev bs=1m count=600 | pv -s $[ count * blocksize ] > /dev/null
+# 1m or 1M ?
+
+if dd count=1 if=/dev/null of=/dev/null bs=1M >/dev/null 2>&1; then
+    onemeg=1M;
+else
+    if dd count=1 if=/dev/null of=/dev/null bs=1m >/dev/null 2>&1; then
+	onemeg=1m;
+    else
+	echo "dd is wonky and doesn't know megabytes?  Sorry."
+	exit 1
+    fi
+fi
+
+# now, which device?
+
+if [ -b "$dev" ]; then
+    disk="$dev";
+elif [ -b /dev/$dev ]; then
+    disk=/dev/$dev;
+elif [ -b /dev/r$dev ]; then
+    disk=/dev/r$dev;
+else
+    echo name your disk
+    exit 1
+fi
+
+time dd if=$disk bs=$onemeg count=600 | pv -s $[ count * blocksize ] > /dev/null
