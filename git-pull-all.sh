a531068b7c11d0f759a43102aa20b9ca097b4c7d
Todd Kaufmann <toddkaufmann+g@gmail.com>
2021-05-14 11:13:32 -0400

    now detects whether there were changes before stopping

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -8,16 +8,24 @@ function pull-all() {
 	if [ -d "$repo/.git" ]; then
 	    echo $(date) ============================================ $repo;
 	    if cd $repo; then
-		if ! git pull; then
-		    echo git pull FAIL - $?
+		if ! git fetch; then
+		    echo git fetch FAIL - $?
 		    sleep 2
 		else
 		    # how to tell if changes ?
-		    echo check it out -
-		    sleep 1
-		    git lol | head
-		    echo
-		    read -p "<<any char to proceed>> " -n 1
+		    if git status -uno  | grep 'Your branch is up to date'; then
+			echo up to date ...................................;
+		    else
+			echo check it out -
+			sleep 1
+			if ! git pull; then
+			    echo pull FAIL -  $!
+			fi
+			echo
+			git lol | head
+			echo
+			read -p " << any char to proceed>> " -n 1
+		    fi
 		    echo
 		fi
 		cd ..;
